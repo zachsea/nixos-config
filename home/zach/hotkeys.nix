d7d6e8090f4bc98843d3e744a5ca78954b0cfad7
zachsea <zachse4@gmail.com>
2026-09-07 21:00:31 -0700

    screenshot hotkey

--- home/zach/hotkeys.nix
+++ home/zach/hotkeys.nix
@@ -1,13 +1,21 @@
 {
   programs.plasma = {
     enable = true;
-    hotkeys.commands."vicinae" = {
-      name = "Vicinae";
-      key = "Meta+Space";
-      command = "vicinae toggle";
+    hotkeys.commands = {
+      "vicinae" = {
+        name = "Vicinae";
+        key = "Meta+Space";
+        command = "vicinae toggle";
+      };
+      "flameshot-gui" = {
+        name = "Flameshot Capture";
+        key = "Ctrl+Print";
+        command = "flameshot gui";
+      };
     };
     shortcuts = {
       "services/plasma-manager-commands.desktop".vicinae = "Meta+Space";
+      "services/plasma-manager-commands.desktop"."flameshot-gui" = "Ctrl+Print";
     };
   };
 }
