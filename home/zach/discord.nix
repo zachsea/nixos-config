2ce9a782b00e9b75c76a9aa46a9078b723b3b122
zachsea <zachse4@gmail.com>
2026-09-12 17:38:08 -0700

    fix discord streams

--- home/zach/discord.nix
+++ home/zach/discord.nix
@@ -1,5 +1,30 @@
+{ pkgs, ... }:
+let
+  discord-egl = pkgs.symlinkJoin {
+    name = "discord-egl";
+    paths = [ pkgs.discord ];
+    buildInputs = [ pkgs.makeWrapper ];
+    postBuild = ''
+      wrapProgram $out/bin/discord --add-flags "--use-gl=egl"
+    '';
+  };
+in
 {
   programs.discord = {
     enable = true;
+    package = discord-egl;
+  };
+
+  xdg.desktopEntries.discord = {
+    name = "Discord";
+    genericName = "Internet Messenger";
+    exec = "${discord-egl}/bin/discord %U";
+    icon = "discord";
+    type = "Application";
+    categories = [
+      "Network"
+      "InstantMessaging"
+    ];
+    mimeType = [ "x-scheme-handler/discord" ];
   };
 }
